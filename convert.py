e9f3ea1b11d63a914ee98b1e44c54a58d183e4a5
Christian Schneeweiss <christianschneeweiss@icloud.com>
2019-05-04 20:43:29 +0200

    extract messages from txt file

---
+++ convert.py
@@ -0,0 +1,32 @@
+import sys
+
+
+def is_line_new_message(line: str) -> bool:
+    return line.startswith("[")
+
+
+def main():
+    filepath = sys.argv[1]
+    with open(filepath) as fp:
+        messages = extract_messages(fp)
+        print(messages)
+
+
+def extract_messages(fp) -> [str]:
+    messages = []
+    line = fp.readline()
+    message = ""
+    while line:
+        if not is_line_new_message(line):
+            message += ". " + line
+        else:
+            messages.append(message)
+            message = line
+        message = message.strip()
+        line = fp.readline()
+    return messages
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main()
